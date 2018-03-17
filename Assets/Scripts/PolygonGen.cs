7e7f9e5640a35836ac7c607c6fabb2abb77872db
Adam Weider <ahw214@uchicago.edu>
2018-03-17 18:31:20 -0500

    Working on UI for using tower generator.

---
+++ Assets/Scripts/PolygonGen.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Math = System.Math;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PolygonGen : MonoBehaviour
+{
+    public SteamVR_TrackedObject controllerR;
+    public Material material;
+    private GameObject polygon;
+    private double init, final;
+    private int n = 4, n2;
+
+    TowerGen towerGen;
+    
+    void Start ()
+    {
+        towerGen = GetComponent<TowerGen>();
+        Mesh mesh = PolyGen(n, 0.06);
+
+        polygon = new GameObject("Polygon");
+        polygon.transform.position = controllerR.transform.position + 0.06F * controllerR.transform.up;
+        polygon.transform.SetParent(controllerR.transform);
+        polygon.AddComponent<MeshFilter>();
+        polygon.AddComponent<MeshRenderer>();
+        polygon.GetComponent<Renderer>().material = material; 
+        polygon.GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    void Update()
+    {
+        var deviceR = SteamVR_Controller.Input((int)controllerR.index);
+
+        if (deviceR.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            init = deviceR.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+        }
+
+        else if (deviceR.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            final = deviceR.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+
+            double disp = 3 * (final - init);
+            n2 = n + (int)Math.Round(disp);
+
+            if(n2 >= 3)
+                polygon.GetComponent<MeshFilter>().mesh = PolyGen(n2, 0.06);   
+        }
+
+        else if (deviceR.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            if (n2 >= 3)
+            {
+                n = n2;
+                towerGen.BuildTower(n);
+            }
+            init = 0;
+        }
+    }
+
+    Mesh PolyGen(int n, double r)
+    {
+        double s = r * 2 * Math.Sin(Math.PI / n);
+        double angle = Math.PI / 2;
+        double angle_sym = (2 * Math.PI) / n;
+
+        Vector3[] verts = new Vector3[n];
+
+        for (int i = 0; i < verts.Length; i++)
+        {
+            float x = (float)(r * Math.Cos(angle));
+            float z = (float)(r * Math.Sin(angle));
+            verts[i] = new Vector3(x, 0, z);
+            angle += angle_sym;
+        }
+
+        int[] tris = new int[3 * (n - 2)];
+        int vert = n - 1;
+
+        for (int i = 0; i < tris.Length; i += 3)
+        {
+            tris[i] = 0;
+            tris[i + 1] = vert;
+            tris[i + 2] = vert - 1;
+
+            vert--;
+        }
+
+        Mesh mesh = new Mesh
+        {
+            vertices = verts,
+            triangles = tris
+        };
+
+        return mesh;
+    }
+}
--- Assets/Scripts/TowerGen.cs
+++ Assets/Scripts/TowerGen.cs
@@ -14,55 +14,63 @@ public class TowerGen : MonoBehaviour {
     public int tower_sides;
     public int tower_height;
 
-    public float offset;
+    public float block_offset;
 
 	void Start ()
     {
-        GameObject tower = new GameObject();
+        BuildTower(tower_sides, tower_height, block_length, block_width, block_height, block_offset);
+	}
+
+    public void BuildTower(int sides, int t_height = 15, float length = 7, float width = 0.5F, float b_height = 1, float offset = 1.1F)
+    {
+        GameObject tower = GameObject.Find("Tower");
+        if (tower)
+            Destroy(tower);
+
+        tower = new GameObject("Tower");
         tower.transform.SetParent(GameObject.Find("Tower Scale Origin").transform);
-        tower.name = "Tower";
 
-        GameObject floor = new GameObject();
+        GameObject floor = new GameObject("Floor 1");
         floor.transform.SetParent(tower.transform);
-        floor.name = "Floor 1";
 
-        prefab.transform.localScale = new Vector3(block_width, block_height, block_length);
+        prefab.transform.localScale = new Vector3(width, b_height, length);
 
-        Vector3 init_pos = new Vector3(0, block_height / 2, 0);
+        Vector3 init_pos = new Vector3(0, b_height / 2, 0);
         Quaternion init_rot = prefab.transform.rotation;
 
-        float apothem = (float)(block_length / (2 * Math.Tan(Math.PI / tower_sides)));
-        float exterior_angle = To_deg(Math.PI / tower_sides);
+        float apothem = (float)(length / (2 * Math.Tan(Math.PI / sides)));
+        float exterior_angle = To_deg(Math.PI / sides);
 
         float new_angle;
         Vector3 new_pos;
         Quaternion new_rot;
 
-        for (int s = 0; s < tower_sides; s++)
+        for (int s = 0; s < sides; s++)
         {
             new_angle = 2 * s * exterior_angle;
             new_rot = init_rot * Quaternion.Euler(0, new_angle, 0);
             new_pos = init_pos + new_rot * new Vector3(offset * apothem, 0, 0);
 
             GameObject block = GameObject.Instantiate(prefab, new_pos, new_rot, floor.transform);
-            // block.name = "Block " + (s + 1);
+            block.name = "Block " + (s + 1);
         }
 
-        init_pos += new Vector3(0, block_height/2, 0);
+        init_pos += new Vector3(0, b_height / 2, 0);
 
-        for (int h = 1; h < tower_height; h++)
+        for (int h = 1; h < t_height; h++)
         {
             init_rot *= Quaternion.Euler(0, exterior_angle, 0);
             GameObject floor_dupe = GameObject.Instantiate(floor, init_pos, init_rot, tower.transform);
             floor_dupe.name = "Floor " + (h + 1);
 
-            init_pos += new Vector3(0, block_height, 0);
+            init_pos += new Vector3(0, b_height, 0);
         }
-		
-	}
+    }
 
     float To_deg(double rad)
     {
         return (float)(rad * (180 / Math.PI));
     }
 }
+
+
